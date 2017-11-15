af2a930d33005dec2ffda7720dbd7a1972d3ab73
Irakli <i.dzigrashvili@gmail.com>
2017-11-16 00:19:03 +0400

    two

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { CourseService } from './courses.service';
 
 
 @Component ({
@@ -14,6 +15,11 @@ import {Component} from '@angular/core';
 export class CoursesComponent {
 
     title = 'List of courses';
-    courses = ['course1', 'course2', 'course3' ];
+    courses;
+
+    constructor( service: CourseService) {
+        
+        this.courses = service.getCourses();
+    }
 
 }
