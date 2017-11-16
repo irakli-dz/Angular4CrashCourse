a9aba83dd042948cbfa91ee8dfe2adaf28682324
Irakli <i.dzigrashvili@gmail.com>
2017-11-16 23:36:19 +0400

    Two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CourseService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors/authors.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [
-    CourseService
+    CourseService,
+    AuthorsService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,13 +4,7 @@ import { CourseService } from './courses.service';
 
 @Component ({
     selector: 'courses',
-    template: `<h2>{{title}}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-        {{course}}
-        </li>
-    </ul>
-    `
+    template: `<button class="btn btn-primary">Save</button>`
 })
 export class CoursesComponent {
 
